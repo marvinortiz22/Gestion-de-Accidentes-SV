b72382b7cc466115c7622b3deeda83b2f10f4016
marvinortiz22 <104816517+marvinortiz22@users.noreply.github.com>
2022-06-20 15:32:57 -0600

    Update admin.py

--- gestionAccidentes/admin.py
+++ gestionAccidentes/admin.py
@@ -5,13 +5,13 @@ class ReporteAdmin(admin.ModelAdmin):
     list_display=('id','accidente','user')
     search_fields=('accidente__id',)
 class AccidenteAdmin(admin.ModelAdmin):
-    list_display=('id','departamento','municipio','lugar','descripcion','fecha')
+    list_display=('id','departamento','municipio','lugar','descripcion','fecha','user')
     search_fields= ('id','departamento','municipio','lugar','descripcion','latitud','longitud')
     list_filter= ('departamento','municipio')
     date_hierarchy="fecha"
 class DatosExtraAdmin(admin.ModelAdmin):
-    list_display=('id','accidente','cantVictimas','foto')
-    search_fields= ('id','cantVictimas','foto')
+    list_display=('id','accidente','cantVictimas','foto',)
+    search_fields= ('id','cantVictimas','foto','accidente__id')
     list_filter=('accidente'),
 admin.site.register(Reporte, ReporteAdmin)
 admin.site.register(ReporteAccidente, AccidenteAdmin)
